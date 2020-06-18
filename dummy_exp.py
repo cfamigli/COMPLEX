c5410f450e64418d03e9f6887d841568a31a565d
cfamigli <cfamigli@stanford.edu>
2020-06-18 12:29:48 -0700

    adding dummy experiment

---
+++ dummy_exp.py
@@ -0,0 +1,50 @@
+
+import os
+import sys
+import complex_utilities as computil
+from pandas import read_csv, DataFrame
+import numpy as np
+import glob
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def plot_corr(corr, model, type='', dummy=0, dimensionality=''):
+    plt.figure(figsize=(7,7))
+    cmap = plt.cm.get_cmap('bwr_r')
+    sns.heatmap(corr, mask=np.triu(np.ones_like(corr, dtype=np.bool)), cmap=cmap, vmin=-1, vmax=1, center=0,
+        square=True, linewidths=.5, cbar_kws={"shrink": .5})
+    plt.title(model + '_' + type + '\nnumber of dummy pars = ' + str(dummy) + '\ndimensionality = ' + str(dimensionality))
+    plt.tight_layout()
+    plt.savefig('../../../../plots/heatmaps/dimensionality/' + model + '_' + type + '_' + str(dummy) + '.pdf')
+    return
+
+def main():
+    model = 'C1'
+    os.chdir('../data/COMPLEX_v1.4/' + model + '/US-Ha1_EDC/')
+
+    exp = '1c'
+    posterior = computil.csv_to_np(glob.glob('*exp' + exp + 'EDC*parameters_*.csv')[0], header=None)
+    posterior_dimensionality = computil.do_PCA(posterior, posterior.shape[1])
+    plot_corr(read_csv(glob.glob('*exp' + exp + 'EDC*parameters_*.csv')[0], header=None).corr(), model, type='posterior_exp' + exp,
+        dummy=0, dimensionality=posterior_dimensionality)
+
+    exp = '1f'
+    prior = computil.csv_to_np(glob.glob('*exp' + exp + 'EDC*parameters_*.csv')[0], header=None)
+    prior_dimensionality = computil.do_PCA(prior, prior.shape[1])
+    plot_corr(DataFrame.from_records(prior).corr(), model, type='prior', dummy=0, dimensionality=prior_dimensionality)
+
+    for i in range(1,10):
+        prior = np.append(prior, np.random.uniform(size=prior.shape[0]).reshape((-1,1)), 1)
+        prior_dimensionality = computil.do_PCA(prior, prior.shape[1])
+        plot_corr(DataFrame.from_records(prior).corr(), model, type='prior_dummy', dummy=i, dimensionality=prior_dimensionality)
+
+        posterior = np.append(posterior, np.random.uniform(size=posterior.shape[0]).reshape((-1,1)), 1)
+        posterior_dimensionality = computil.do_PCA(posterior, posterior.shape[1])
+        plot_corr(DataFrame.from_records(posterior).corr(), model, type='posterior_dummy', dummy=i, dimensionality=posterior_dimensionality)
+
+    return
+
+if __name__=='__main__':
+    main()
